99a6a3efc0e86f58e89cba372e7542d5a637bf84
Ness <whistler990@gmail.com>
2019-11-17 13:58:48 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
     text-transform: capitalize;
-    font-size: 1.3rem;
+    font-size: 1.4rem;
     background: transparent;
     cursor: pointer;
     padding: 0.2rem 0.5rem;
